27f394c36c82848ea6caa98e4478c7cf7b2c012e
hoonkim <hoonk9387@gmail.com>
2023-01-23 19:52:51 -0600

    Created definition of same function with no body.

---
+++ 01_20230123.js
@@ -0,0 +1,9 @@
+// Problem: Write a function called same, which accepts two arrays.
+//          The function should return true if every value in the 
+//          array has it's corresponding value squared in the 
+//          second array. The frequency of the values must be 
+//          the same.
+
+function same(arr1, arr2) {
+    
+}
