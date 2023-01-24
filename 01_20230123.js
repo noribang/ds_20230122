2313b1ad8d1da062d878f23ae6af5deec422268d
hoonkim <hoonk9387@gmail.com>
2023-01-23 20:04:54 -0600

    Added conditional control of flow that checks if arrays' lengths are equal.

--- 01_20230123.js
+++ 01_20230123.js
@@ -5,11 +5,19 @@
 //          the same.
 
 function same(arr1, arr2) {
-    console.log(arr1)
-    console.log(arr2)
+    // console.log(arr1)
+    // console.log(arr2)
+    if (arr1.length === arr2.length) {
+        return true
+    } else {
+        return false
+    }
+
 }
 
 array1 = [1, 2, 3]
 array2 = [1, 4, 9]
+array3 = [1, 4, 9, 16]
 
 same(array1, array2)
+same(array1, array3)
