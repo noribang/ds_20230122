c509754d1879a89a083ca76d1c5335f643d1982b
hoonkim <hoonk9387@gmail.com>
2023-01-25 20:56:59 -0600

    In function added test to see if arr1 length and arr2 length are equal.

--- 02_same_refactored.js
+++ 02_same_refactored.js
@@ -4,5 +4,15 @@
 // Second array must be values of first array squared.
 
 function same(arr1, arr2) {
+    // Check for frequency of values.
+    if (arr1.length !== arr2.length) {
+        return true;
+    }
 
+    return true;
 }
+
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+
+same(array1, array2)
