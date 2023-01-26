f5637ef2ce905cf84e0e01103da1989bb5e57a3c
hoonkim <hoonk9387@gmail.com>
2023-01-25 21:24:15 -0600

    Added objects in body to hold frequency of array elements.

--- 02_same_refactored.js
+++ 02_same_refactored.js
@@ -8,7 +8,14 @@ function same(arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return true;
     }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
 
+    console.log(frequencyCounter1)
+    // console.log(frequencyCounter2)
     return true;
 }
 
