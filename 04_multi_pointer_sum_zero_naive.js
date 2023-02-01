2eeb927d340ca1c933ccf7746bcaf599bcd60011
hoonkim <hoonk9387@gmail.com>
2023-01-31 20:02:03 -0600

    Added two nested loops that will iterate through array.

--- 04_multi_pointer_sum_zero_naive.js
+++ 04_multi_pointer_sum_zero_naive.js
@@ -6,5 +6,14 @@
 // Function should find first pair that sum to zero.
 // Return an array that includes both values that 
 // sum to zero or undefined if a pair does not exist.
+function sumZero(arr) {
+    // Iterate through array starting at index 0 to the end.
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+
+        }
+    }
+
+}
 
 sumZero([-4,-3,-2,-1,0,1,2,5])
