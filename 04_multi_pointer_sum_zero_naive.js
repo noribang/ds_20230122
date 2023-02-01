c6f9021af9e01fb76a11ee44aeff45a72ef855f7
hoonkim <hoonk9387@gmail.com>
2023-01-31 20:21:53 -0600

    Added if condition to find two integers that sum to zero.

--- 04_multi_pointer_sum_zero_naive.js
+++ 04_multi_pointer_sum_zero_naive.js
@@ -6,12 +6,21 @@
 // Function should find first pair that sum to zero.
 // Return an array that includes both values that 
 // sum to zero or undefined if a pair does not exist.
+
+// Function starts from first integer on left and 
+// successively adds to itself the number to the
+// right. If test finds two integers that sum to 
+// zero it will return array with those two integers. 
 function sumZero(arr) {
     // Iterate through array starting at index 0 to the end.
     for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
+        // console.log(arr[i]);
+        // Start iterating from the immediate right of i index.
         for (let j = i + 1; j < arr.length; j++) {
-            console.log(arr[j])
+            // console.log(arr[j])
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]]
+            }
         }
     }
 
