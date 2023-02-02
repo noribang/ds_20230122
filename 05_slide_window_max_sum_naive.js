8de6ad575a339e7aaeff95fb0448e3eb7e6b142a
hoonkim <hoonk9387@gmail.com>
2023-02-01 21:08:12 -0600

    Added if condition to body of function in first slide window pattern file.

--- 05_slide_window_max_sum_naive.js
+++ 05_slide_window_max_sum_naive.js
@@ -1,7 +1,10 @@
 // Problem: Find subset of data in a larger set of data.
 // e.g. Calculate sum of n consecutive elements in an array.
 function maxSubarraySum(arr, num) {
-
+    if (num > arr.length) {
+        return null;
+    }
+    
 }
 
 
