05858e907eefdf75b151074a4b87a5e40a81dcc1
hoonkim <hoonk9387@gmail.com>
2023-02-02 20:47:35 -0600

    Created function declaration with two parameters.

--- 05_slide_window_max_sum_naive.js
+++ 05_slide_window_max_sum_naive.js
@@ -1,12 +1,9 @@
 // Problem: Find subset of data in a larger set of data.
 // e.g. Calculate sum of n consecutive elements in an array.
-function maxSubarraySum(arr, num) {
-    if (num > arr.length) {
-        return null;
-    }
-    
-}
 
+function maxSubarraySum(arr, num) {
+
+}
 
 
 maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
