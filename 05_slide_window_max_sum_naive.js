e292f8697e2764a3b8fccc777414a2368a6b6b2a
hoonkim <hoonk9387@gmail.com>
2023-02-02 21:12:49 -0600

    Added inner loop. This calculates sum of subset of array from left to right.

--- 05_slide_window_max_sum_naive.js
+++ 05_slide_window_max_sum_naive.js
@@ -9,9 +9,15 @@ function maxSubarraySum(arr, num) {
     }
     // 
     let max = -Infinity;
-    // 
+    // Creates window or subset of array.
     for (let i = 0; i < arr.length - num + 1; i++) {
-        
+        // e.g. 9-3+1=7
+        // e.g. 9-2+1=8
+        temp = 0;
+        // 
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j]
+        }
 
     }
 
@@ -19,3 +25,4 @@ function maxSubarraySum(arr, num) {
 
 maxSubarraySum([],3)
 maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+maxSubarraySum([2,6,9,2,1,8,5,6,3],2)
