b1f3b2ac91be9ca2373a0f61332774925cac19c3
hoonkim <hoonk9387@gmail.com>
2023-02-03 21:29:25 -0600

    Added start of function declaration.

--- 05_slide_window_max_sum_naive.js
+++ 05_slide_window_max_sum_naive.js
@@ -1,25 +1,7 @@
 // Problem: Find subset of data in a larger set of data.
-// e.g. Calculate sum of n consecutive elements in an array.
+// e.g. Calculate greatest sum of n consecutive elements in an array.
 
 function maxSubarraySum(arr, num) {
-    // Check if num subset of array is greater than array. 
-    // Return null if true.
-    if (num > arr.length) {
-        return null
-    }
-    // 
-    let max = -Infinity;
-    // Creates window or subset of array.
-    for (let i = 0; i < arr.length - num + 1; i++) {
-        // e.g. 9-3+1=7
-        // e.g. 9-2+1=8
-        temp = 0;
-        // 
-        for (let j = 0; j < num; j++) {
-            temp += arr[i + j]
-        }
-
-    }
 
 }
 
