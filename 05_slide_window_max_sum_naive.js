1dc895e09ad9662165c2a8c7d9f38fb8f3119265
hoonkim <hoonk9387@gmail.com>
2023-02-03 21:49:07 -0600

    Added outer for loop.

--- 05_slide_window_max_sum_naive.js
+++ 05_slide_window_max_sum_naive.js
@@ -2,6 +2,17 @@
 // e.g. Calculate greatest sum of n consecutive elements in an array.
 
 function maxSubarraySum(arr, num) {
+    // Check if subset testing for is greater than array itself.
+    if (num > arr.length) {
+        return null;
+    } 
+    // Value to check each subset against.
+    let max = -Infinity
+    // Sliding window for loop. 
+    for (let i = 0; i < arr.length - num + 1; i++) {
+
+    }
+
 
 }
 
