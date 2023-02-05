d65abd0567121dd6f6098f6b289a28ae4a59da2c
hoonkim <hoonk9387@gmail.com>
2023-02-05 11:08:10 -0600

    Added for loop and if condition in search function.

--- 07_divide_and_conquer_eamples.js
+++ 07_divide_and_conquer_eamples.js
@@ -1,5 +1,17 @@
 // Search for a value in array and return the index.
 // Loop through an array from left to right until value is found.
-function search() {
-
+// Return index when found.
+function search(arr, val) {
+    // Loop through array from left to right.
+    for (let i = 0; i < arr.length; i++) {
+        // If value found return its index.
+        if (arr[i] === val) {
+            return i;
+        }
+    }
+    // If value not found return -1;
+    return -1;
 }
+
+let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
+search(arrSearch, 3)
