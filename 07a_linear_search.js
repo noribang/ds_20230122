52b5a1212bfbd63a1b430783da5fd52bdb1bf247
hoonkim <hoonk9387@gmail.com>
2023-02-06 19:40:30 -0600

    Added linear search. Started linear search function declaration.

---
+++ 07a_linear_search.js
@@ -0,0 +1,11 @@
+// Linear search
+// O(n) linear time.
+// Array must be sorted.
+
+// Return index of val in arr.
+function linearSearch(arr, val) {
+
+}
+
+let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
+search(arrSearch, 3)
