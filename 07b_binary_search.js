9b0a06b23f284353b913823e79e012f1c5642928
hoonkim <hoonk9387@gmail.com>
2023-02-07 21:06:15 -0600

    Declare middle.

--- 07b_binary_search.js
+++ 07b_binary_search.js
@@ -6,6 +6,13 @@ function binarySearch(arr, val) {
     // 
     let min = 0;
     let max = arr.length - 1;
-    // 
+    // Cut search in half on each iteration.
+    while (min <= max) {
+        // Variable for middle.
+        let middle = Math.floor((min + max) / 2);
+        // Get middle element in array.
+        let currentElement = arr[middle];
+
+    }
 
 }
