a81cb14c8754e0e4f4f08817a59157fb67582f49
hoonkim <hoonk9387@gmail.com>
2023-02-08 22:31:44 -0600

    Started function declaration.

--- 07b_binary_search.js
+++ 07b_binary_search.js
@@ -3,16 +3,5 @@
 // Sorted.
 
 function binarySearch(arr, val) {
-    // 
-    let min = 0;
-    let max = arr.length - 1;
-    // Cut search in half on each iteration.
-    while (min <= max) {
-        // Variable for middle.
-        let middle = Math.floor((min + max) / 2);
-        // Get middle element in array.
-        let currentElement = arr[middle];
-
-    }
 
 }
