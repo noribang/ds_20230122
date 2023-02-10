d49bf9f88e5d70a0906e104a988db6cf7687e53b
hoonkim <hoonk9387@gmail.com>
2023-02-09 20:36:02 -0600

    Started function declaration for linear search.

--- 07a_linear_search.js
+++ 07a_linear_search.js
@@ -2,6 +2,12 @@
 // O(n) time complexity.
 // Array must be sorted.
 
+// 
+function linearSearch(arr, val) {
+    
+
+
+}
 
 let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
 linearSearch(arrSearch, 3)
