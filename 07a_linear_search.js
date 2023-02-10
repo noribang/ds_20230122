8b538e23dc9d1669a9cfc7fae97f488b3716e0b1
hoonkim <hoonk9387@gmail.com>
2023-02-09 20:50:58 -0600

    Added test condition to return index if val is found.

--- 07a_linear_search.js
+++ 07a_linear_search.js
@@ -8,11 +8,16 @@ function linearSearch(arr, val) {
     for (let i = 0; i < arr.length; i++) {
         // 
         let current = arr[i]
-        console.log("current: ", current)
+        // console.log("current: ", current)
+        // 
+        if (current === val) {
+            // return index i if equal to val
+            return i;
+        }
     }
-
-
+    // Default return -1 if not found.
+        return -1;
 }
 
 let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
-linearSearch(arrSearch, 3)
+linearSearch(arrSearch, 12)
