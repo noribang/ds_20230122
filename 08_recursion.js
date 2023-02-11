531a06a19dc9e284ee978b2ce35aa0527d91d6f4
hoonkim <hoonk9387@gmail.com>
2023-02-11 15:55:13 -0600

    Added the base case and recursive case logic to function.

--- 08_recursion.js
+++ 08_recursion.js
@@ -3,6 +3,16 @@
 // 2) Recursive case. Different input.
 
 // Prints numbers in decreasing order.
-function countDown() {
-
+function countDown(num) {
+    // Base case.
+    if (num <= 0) {
+        console.log("All done!");
+        return;
+    }
+    // Recursive case.
+    console.log(num);
+    // num--;
+    countDown(num - 1);
 }
+
+countDown(11);
