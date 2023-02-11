80f82343b5678f31b5fc7ad1d7f3ada9c2001e3e
hoonkim <hoonk9387@gmail.com>
2023-02-10 21:05:10 -0600

    Added test to check if current element in loop is equal to val looking for.

--- 07a_linear_search.js
+++ 07a_linear_search.js
@@ -7,11 +7,20 @@ function linearSearch(arr, val) {
     // 
     for (let i = 0; i < arr.length; i++) {
         let current = arr[i];
-        console.log(current);
+        // console.log(current);
+        // If current element in array equals
+        // val that we are looking for 
+        // return index i.
+        if (current === val) {
+            return i;
+        }       
     }
-
+    // Default
+    // If current is never equal to val
+    // return -1.
+    return -1
 }
 
 
 let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
-linearSearch(arrSearch, 12)
+linearSearch(arrSearch, 46)
