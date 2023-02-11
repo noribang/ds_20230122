461ad4d987bfdc572c4a6ef39ab72cf796103728
hoonkim <hoonk9387@gmail.com>
2023-02-10 20:47:14 -0600

    Started function declaration for linear search.

--- 07a_linear_search.js
+++ 07a_linear_search.js
@@ -2,22 +2,11 @@
 // O(n) time complexity.
 // Array must be sorted.
 
-// 
-function linearSearch(arr, val) {
-    // 
-    for (let i = 0; i < arr.length; i++) {
-        // 
-        let current = arr[i]
-        // console.log("current: ", current)
-        // 
-        if (current === val) {
-            // return index i if equal to val
-            return i;
-        }
-    }
-    // Default return -1 if not found.
-        return -1;
+// Find value by iterating through array from left to right.
+function linearSearch() {
+
 }
+
 
 let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
 linearSearch(arrSearch, 12)
