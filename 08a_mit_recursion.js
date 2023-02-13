2589af7b0c51e15902551b0225badfd32f6d96cc
hoonkim <hoonk9387@gmail.com>
2023-02-12 18:40:22 -0600

    Created iterative function to multiply a, b times.

--- 08a_mit_recursion.js
+++ 08a_mit_recursion.js
@@ -10,3 +10,12 @@
 // stop i when i = 0
 // Value of computation: result
 // result = result + a
+function mult_iter(a, b) {
+    result = 0;
+    while (b > 0) {
+        result = result + a
+        b = b - 1;
+    }
+    return result;
+}
+mult_iter(3, 5)
