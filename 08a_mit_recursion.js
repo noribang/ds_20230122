ea05d8fce027bc099c5f658e720b09304c0668d5
hoonkim <hoonk9387@gmail.com>
2023-02-12 18:16:05 -0600

    Writing out process of multiplying a, b times.

---
+++ 08a_mit_recursion.js
@@ -0,0 +1,12 @@
+// 1) Multiplication
+// a. a * b
+// b. add a to itself b times
+// a + a + a...+ a 
+// Capture state by:
+// Iteration i
+// i = b
+// decrement i
+// i = i - 1 
+// stop i when i = 0
+// Value of computation: result
+// result = result + a
