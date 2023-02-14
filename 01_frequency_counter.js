e9d07b379523666c9ccb0b5ae7cfa50fbaf1a4b8
hoonkim <hoonk9387@gmail.com>
2023-02-13 22:27:44 -0600

    Added nested loop to test if two arrays have same frequency of values.

--- 01_frequency_counter.js
+++ 01_frequency_counter.js
@@ -8,9 +8,18 @@ function same(arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return false;
     }   
-
+    // Test if each value in arr1 exists in arr2
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i])
+        if (correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex, 1)
+    }
+    return true;
 }
 
 test1 = [1,2,3,4,5]
-test2 = [9,25,16,4,1]
+// test2 = [9,25,16,4,1]
+test2 = [1,2,3,4,5]
 same(test1, test2)
