703e1380f1f37fedfa905cc4aea9474940e28321
hoonkim <hoonk9387@gmail.com>
2023-02-13 22:16:05 -0600

    Added if condition to test if two arrays are not the same length.

--- 01_frequency_counter.js
+++ 01_frequency_counter.js
@@ -4,5 +4,13 @@
 // Compare two arrays of same frequency of values.
 
 function same(arr1, arr2) {
+    // Check if two arrays are not same length.
+    if (arr1.length !== arr2.length) {
+        return false;
+    }   
 
 }
+
+test1 = [1,2,3,4,5]
+test2 = [9,25,16,4,1]
+same(test1, test2)
