ae148a4c3051ffbdecb8f96de4a3455a055d76fc
hoonkim <hoonk9387@gmail.com>
2023-02-14 20:54:42 -0600

    Added comments to frequency counter iterative function.

--- 01_frequency_counter.js
+++ 01_frequency_counter.js
@@ -1,23 +1,10 @@
-// O(n^2) time complexity.
+// O(n^2) Quadratic time complexity.
 // Nested loop using indexOf.
 
 // Compare two arrays of same frequency of values.
+// Same length array. Same values in array.
 
-function same(arr1, arr2) {
-    // Check if two arrays are not same length.
-    if (arr1.length !== arr2.length) {
-        return false;
-    }   
-    // Test if each value in arr1 exists in arr2
-    for (let i = 0; i < arr1.length; i++) {
-        let correctIndex = arr2.indexOf(arr1[i])
-        if (correctIndex === -1) {
-            return false;
-        }
-        arr2.splice(correctIndex, 1)
-    }
-    return true;
-}
+
 
 test1 = [1,2,3,4,5]
 // test2 = [9,25,16,4,1]
