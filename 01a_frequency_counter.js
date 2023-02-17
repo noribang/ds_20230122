6d07d19e52103efb8620129510927327dea957c8
hoonkim <hoonk9387@gmail.com>
2023-02-16 22:53:03 -0600

    Added two for of loops to populate objects with frequency of values.

--- 01a_frequency_counter.js
+++ 01a_frequency_counter.js
@@ -11,6 +11,14 @@ function same(arr1, arr2) {
     // Create two object literals.
     frequencyCounter1 = {};
     frequencyCounter2 = {};
+    // 
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+    console.log(frequencyCounter2);
 
 }
 
