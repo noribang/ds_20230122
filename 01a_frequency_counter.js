cd9314b49b1cba2535b24ebceff19c0c8bfb66e7
hoonkim <hoonk9387@gmail.com>
2023-02-16 22:21:20 -0600

    Started function declaration with two parameters.

--- 01a_frequency_counter.js
+++ 01a_frequency_counter.js
@@ -1,19 +1,9 @@
 // O(n) linear time complexity.
 
 function same(arr1, arr2) {
-    // Test length of both arrays.
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-    // Two object literals.
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
-    // Populate objects with frequency of each value in array.
-    for (let val of arr1) {
-        console.log(val)
-    }
 
 }
+
 
 test1 = [1,2,3,4,5]
 test2 = [9,25,16,4,1]
