57e498b099caf38be81bee4fb1d8a85f4802cd9b
hoonkim <hoonk9387@gmail.com>
2023-02-17 19:57:02 -0600

    Added function declaration of frequency counter.

--- 01a_frequency_counter.js
+++ 01a_frequency_counter.js
@@ -3,26 +3,15 @@
 // and frequency of values.
 
 function same(arr1, arr2) {
-    // Check lengths of two arrays.
-    // If not same length return false.
+    // Check length of both arrays.
+    // Return if lengths not equal.
     if (arr1.length !== arr2.length) {
         return false;
-    } 
-    // Create two object literals.
-    frequencyCounter1 = {};
-    frequencyCounter2 = {};
-    // 
-    for (let val of arr1) {
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
     }
-    for (let val of arr2) {
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
-    }
-    console.log(frequencyCounter2);
-
+    
 }
 
 
-test1 = [1,2,3,4,5]
-test2 = [9,25,16,4,1]
+test1 = [1,2,3,4,5,3]
+test2 = [9,25,16,4,1,9]
 same(test1, test2)
