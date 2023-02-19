d7de9709fdc682e3a67026b58e794bf9a9d78b9f
hoonkim <hoonk9387@gmail.com>
2023-02-18 18:10:00 -0600

    Added comments to test lengths of strings.

--- 01b_anagram_frequency_counter.js
+++ 01b_anagram_frequency_counter.js
@@ -1,7 +1,10 @@
 // Anagram.
-// Same number of values.
+// Same number of values. Check length of strings.
 // Same frequency of values.
 
 function anagram(str1, str2) {
+    // Test length of strings.
+    // Check if both strings are not equal.
+    // If string lengths are not equal return false.
     
 }
