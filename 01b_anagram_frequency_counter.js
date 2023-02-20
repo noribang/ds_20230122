2b79d37f69cb778c0bd72401f2c813cfea95efe4
hoonkim <hoonk9387@gmail.com>
2023-02-19 21:17:56 -0600

    Starting new function to solve anagram problem.

--- 01b_anagram_frequency_counter.js
+++ 01b_anagram_frequency_counter.js
@@ -2,23 +2,8 @@
 // Same number of values. Check length of strings.
 // Same frequency of values.
 
-function anagram(str1, str2) {
-    // If lengths of strings are not equal no longer need to continue.
-    // Test length of strings.
-    // Check if both strings are not equal.
-    // If string lengths are not equal return false.
-    if (str1 !== str2) {
-        return false;
-    }
-    // Loop through each element in str1.
-    // Store to temp variable.
-    // Compare temp to each element in str2.
-    // If temp exists in str2.
+function validAnagram(first, second) {
     
-
-
-    // Retrue if str1 is empty.
-    // return true;
 }
 
 
