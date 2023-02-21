5b14fd5d9aeb5fa5d47e03b350b8eb2c9e58ad28
hoonkim <hoonk9387@gmail.com>
2023-02-20 21:09:56 -0600

    Added nested for loop to start looping at 1 index position to right of i.

--- 04a_multiple_pointers_naive.js
+++ 04a_multiple_pointers_naive.js
@@ -1,7 +1,11 @@
 function sumZero(arr) {
     // Iterate through array starting at 0.
     for (let i= 0; i < arr.length; i++) {
-        console.log(arr[i])
+        // console.log(arr[i])
+        for (let j = i + 1; j < arr.length; j++) {
+            console.log(arr[j])
+
+        }
     }
 
 
