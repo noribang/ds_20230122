4abd5ab6712ce0decff09fb5af22833102f80c26
hoonkim <hoonk9387@gmail.com>
2023-02-21 21:23:51 -0600

    Added variables for two pinters.

--- 04b_multi_pointers_refactored.js
+++ 04b_multi_pointers_refactored.js
@@ -2,5 +2,10 @@
 // O(1) space complexity.
 // Find first two values that equal zero.
 function sumZero(arr) {
+    // First pointer.
+    let left = 0;
+    // Second pointer.
+    let right = arr.length - 1;
+    
 
 }
