cf7bcee9bf323ffbbbe8218311fc77bc5dbe0aa9
hoonkim <hoonk9387@gmail.com>
2023-02-22 20:56:46 -0600

    Started comments for multi pointer function.

--- 04a_multiple_pointers_naive.js
+++ 04a_multiple_pointers_naive.js
@@ -1,15 +1,7 @@
-// Find two values in array that add to zero.
-function sumZero(arr) {
-    // Iterate through array starting at 0.
-    for (let i= 0; i < arr.length; i++) {
-        // console.log(arr[i])
-        for (let j = i + 1; j < arr.length; j++) {
-            // console.log(arr[j])
-            if (arr[i] + arr[j] === 0) {
-                return [arr[i], arr[j]];
-            }
-        }
-    }
-}
+// O(n^2) time complexity. Using nested loops.
+// 0(1) space complexity.
+// Sorted array necessary.
+// Find first two values that equal zero.
+
 
 sumZero([-4,-3,-2,-1,0,1,2,5])
