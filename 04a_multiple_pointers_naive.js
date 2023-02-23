29e63ce6b4ce73b30bad962d6d0ab4a9c45d4507
hoonkim <hoonk9387@gmail.com>
2023-02-22 21:00:04 -0600

    Started pseudocode for looping through array.

--- 04a_multiple_pointers_naive.js
+++ 04a_multiple_pointers_naive.js
@@ -1,7 +1,11 @@
 // O(n^2) time complexity. Using nested loops.
 // 0(1) space complexity.
 // Sorted array necessary.
-// Find first two values that equal zero.
+// Find first two values of array that equal zero.
+function sumZero(arr) {
+    // Iterate through array starting from index 0 until length - 1.
 
+
+}
 
 sumZero([-4,-3,-2,-1,0,1,2,5])
