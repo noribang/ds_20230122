28cbf8dada412df3df236d3612dd11daad8666dd
hoonkim <hoonk9387@gmail.com>
2023-02-23 20:42:06 -0600

    Added pseudo-like code to describe each step of function.

--- 04a_multiple_pointers_naive.js
+++ 04a_multiple_pointers_naive.js
@@ -2,23 +2,17 @@
 // 0(1) space complexity.
 // Sorted array necessary.
 // Find first two values of array that equal zero.
+// e.g. [-4,-3,-2,-1,0,1,2,5]
+
+// Two pointers. Each pointer is a for loop.
+// First pointer starts at index 0 and iterates to the last value of array.
+// Second pointer starts at index 0 + 1 (immedaite right of first pointer.)
+// Second pointer iterates until end of array.
+// First pointer value is added to second pointer value while second pointer 
+// iterates through array.
+// At each iteration of second pointer through array it is added
+// to first pointer and sum is tested to see if sum equals zero. 
 
 
-function sumZero(arr) {
-    // Iterate through array starting from index 0 until length - 1.
-    // Nested loop will iterate through array starting from i+1 until 
-    // find second value summed with first value equals zero.
-    for (let i = 0; i < arr.length; i++) {
-        // console.log(arr[i]);
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === 0) {
-                console.log(arr[i]+ " + " +arr[j])
-                return [arr[i], arr[j]];
-            }
-            
-        }
-    } 
-    return;
-}
 
 sumZero([-4,-3,-2,-1,0,1,2,5])
