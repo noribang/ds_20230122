a4a04e5967b163211902d4c40dcab11192986691
hoonkim <hoonk9387@gmail.com>
2023-03-02 22:32:43 -0600

    Added if condition to check if temp > max. Assign temp to max if true.

--- 05a_slide_window_naive.js
+++ 05a_slide_window_naive.js
@@ -8,11 +8,13 @@ function maxSubarraySum(arr, num) {
     for (let i = 0; i < arr.length - num +1; i++) {
         temp = 0;
         for (let j = 0; j < num; j++) {
-
+            temp = temp + arr[i + j]
         }
-
+        if (temp > max) {
+            max = temp
+        }
     }
-
+    return max;
 }
 
 
