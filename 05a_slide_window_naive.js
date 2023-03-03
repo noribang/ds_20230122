13af1966dc9dbaa1f4565cc6359413c72fec09d5
hoonkim <hoonk9387@gmail.com>
2023-03-02 22:24:29 -0600

    Started outer for loop.

--- 05a_slide_window_naive.js
+++ 05a_slide_window_naive.js
@@ -4,5 +4,14 @@ function maxSubarraySum(arr, num) {
     if (num > arr.length) {
         return null;
     }
+    let max = -Infinity
+    for (let i = 0; i < arr.length - num +1; i++) {
+
+    }
 
 }
+
+
+maxSubarraySum([],3)
+maxSubarraySum([2,6,9,2,1,8,5,6,3,4],3)
+maxSubarraySum([2,6,9,2,1,8,5,6,3,4],2)
