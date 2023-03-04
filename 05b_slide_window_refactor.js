73b5fa59cf9c0a0ce9d7ced6990ceeb754524118
hoonkim <hoonk9387@gmail.com>
2023-03-03 20:13:11 -0600

    Added if condition to check if array is shorter than subset looking for.

--- 05b_slide_window_refactor.js
+++ 05b_slide_window_refactor.js
@@ -1,5 +1,16 @@
 // O(n) linear time complexity.
+// Find consecutive elements of array that equal max sum.
 
 function maxSubarraySum(arr, num) {
+    let maxSum = 0
+    let tempSum = 0
+    // Check if arr.length is shorter 
+    // than subset num we are looking for.
+    if (arr.length < num) return null;
 
 }
+
+
+maxSubarraySum([],3)
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+maxSubarraySum([2,6,9,2,1,8,5,6,3],2)
